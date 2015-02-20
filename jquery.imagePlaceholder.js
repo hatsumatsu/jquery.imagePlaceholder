d824e407078fe5381227840cefd2039d60f63599
HATSUMATSU <mail@hatsumatsu.de>
2015-02-20 14:52:22 +0100

    initial commit
    hello world!

---
+++ jquery.imagePlaceholder.js
@@ -0,0 +1,123 @@
+/**
+ * imagePlaceholder 
+ * 
+ * Based on the plugin boilerplate by Jonathan Nicol
+ * http://jonathannicol.com/blog/2012/05/06/a-jquery-plugin-boilerplate/
+ */
+;( function( $ ) {
+	var pluginName = 'imagePlaceholder';
+
+	function Plugin( element, options ) {
+		var el = element;
+		var $el = $( element );
+
+		options = $.extend( {}, $.fn[pluginName].defaults, options );
+
+		function init() {						
+			buildPlaceholder();
+			
+			hook( 'onInit' );
+		}
+		
+		function buildPlaceholder() {
+			console.log( $el );
+			var images = $el.find( 'img[height][width]' );
+			images.each( function() {
+				var image = $( this );
+							
+				console.log( image );
+							
+				var top = image.position().top;
+				var left = image.position().left;
+							
+				var placeholder = $( '<canvas></canvas>');
+				placeholder
+					.attr( 'class', image.attr( 'class' ) )
+					.attr( 'id', image.attr( 'id' ) )
+					.attr( 'width', image.attr( 'width' ) )
+					.attr( 'height', image.attr( 'height' ) )
+					.addClass( 'placeholder' );
+							
+				image
+					.addClass( 'has-placeholder' )
+					.css( {
+						'left': left + 'px',
+						'top': top + 'px'
+					} );
+							
+					image.after( placeholder );
+				} );	
+		}
+
+		function option( key, val ) {
+			if( val ) {
+				options[key] = val;
+			} else {
+				return options[key];
+			}
+		}
+
+		function destroy() {
+			$el.each( function() {
+				
+				$( this )
+					.find( '.placeholder' )
+					.remove();
+				$( this )
+					.find( '.has-placeholder' )
+					.removeClass( 'has-placeholder' );
+				
+				var el = this;
+				var $el = $( this );
+
+				hook( 'onDestroy' );
+				$el.removeData( 'plugin_' + pluginName );
+			} );
+		}
+
+		function hook( hookName ) {
+			if( options[hookName] !== undefined ) {
+				options[hookName].call( el );
+			}
+		}
+
+		init();
+
+		return {
+			option: option,
+			destroy: destroy
+		};
+	}
+
+	$.fn[pluginName] = function( options ) {
+		if( typeof arguments[0] === 'string' ) {
+			var methodName = arguments[0];
+			var args = Array.prototype.slice.call( arguments, 1 );
+			var returnVal;
+			this.each( function() {
+				if( $.data( this, 'plugin_' + pluginName ) && typeof $.data( this, 'plugin_' + pluginName )[methodName] === 'function' ) {
+					returnVal = $.data( this, 'plugin_' + pluginName )[methodName].apply( this, args );
+				} else {
+					throw new Error( 'Method ' +  methodName + ' does not exist on jQuery.' + pluginName );
+				}
+			} );
+			if( returnVal !== undefined ) {
+				return returnVal;
+			} else {
+				return this;
+			}
+		} else if( typeof options === "object" || !options ) {
+			return this.each( function() {
+				if( !$.data( this, 'plugin_' + pluginName ) ) {
+					$.data( this, 'plugin_' + pluginName, new Plugin( this, options ) );
+				}
+			});
+		}
+	};
+
+	$.fn[pluginName].defaults = {
+		onInit: function() {},
+		onDestroy: function() {}
+	};
+
+} )( jQuery );
---
+++ readme.md
@@ -0,0 +1,67 @@
+# jquery.imagePlaceholder
+
+**! Very beta, use at your own risk !**
+
+Using complex layout algorithms like [Masonry](http://masonry.desandro.com/) with image-heavy content, you usually need something like [imagesLoaded](http://masonry.desandro.com/appendix.html#imagesloaded) to wait for the images to be completely loaded. This is because  image dimensions are – despite using height and width attributes on the `<img>`  – unknown to the browser until loading is completed.
+
+**jquery.imagePlacegolder** takes a different approach to this issue. It replaces each image with an empty `<canvas>` element using the image's values for `width` and `height` attributes and places the image on top of this placeholder. Since `<canvas>` behaves exactly like `<img>` regarding its intrinsic aspect ratio you can start calculating your layout as soon as the DOM is ready. Since the image's classes are copied, all CSS of your images apply to the placeholder as well.
+
+### Pro's
++ Calculate layout before images are loaded
++ Give a visual feedback on the loading process
+
+### Limitations
++ Only works with images that have `width` and `height` attributes (most CMSs can do that for you)
++ images have to be `position: relative` or `static` 
++ **Very beta, use at your own risk**
+
+## Usage 
++ include jquery.imagePlaceholder
++ add the following lines of CSS
+
+```css
+.placeholder {
+  position: relative;
+}
+
+.has-placeholder {
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 10;
+}
+```
+
++ call the plugin like `$( '.container' ).imagePlaceholder()`
+
+### Using jquery.imagePlaceholder width Masonry
+
+Just call imagePlacehgoder befgore you append an item to the masonry layout. 
+So instead of this...
+
+```css
+$( '.item-to-append' )
+  .appendTo( $( '.layout-container' ) );
+
+// might take a while
+$( '.item-to-append' ).imagesLoaded( function() {
+    $( '.layout-container' )
+      .masonry( 'appended', $( '.item-to-append' ) );    
+} );
+```
+
+... do this 
+
+```css 
+$( '.item-to-append' )
+  .appendTo( $( '.layout-container' ) );
+  .imagePlaceholder();
+
+$( '.layout-container' )
+  .masonry( 'appended', $( '.item-to-append' ) );    
+} );
+```
+
+## Credits
++ I used the [jquery plugin boilerplate by Jonathan Nicol](http://jonathannicol.com/blog/2012/05/06/a-jquery-plugin-boilerplate/)
++ Everything started in [here](https://github.com/desandro/imagesloaded/issues/175). 
